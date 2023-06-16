0cab47b08ef578639928c583114c4960db3e1d80
isaac.nascimento <isaacs.nascimento17@gmail.com>
2023-06-16 20:19:12 -0300

    [BUG FIX]: Overflowed pxls by Expanded. Button: 'Criar Conta'

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -13,66 +13,107 @@ class SignInScreen extends StatelessWidget {
           Expanded(
             child: Container(color: Colors.red),
           ),
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 32,
-                vertical: 40,
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 32,
+              vertical: 40,
+            ),
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(45),
               ),
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.vertical(
-                  top: Radius.circular(45),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                //email
+                const CustomTextField(
+                  icon: Icons.email,
+                  label: 'Email',
                 ),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  //email
-                  const CustomTextField(
-                    icon: Icons.email,
-                    label: 'Email',
-                  ),
 
-                  // senha
-                  const CustomTextField(
-                    icon: Icons.lock,
-                    label: 'Senha',
-                    isPasswordField: true,
-                  ),
+                // senha
+                const CustomTextField(
+                  icon: Icons.lock,
+                  label: 'Senha',
+                  isPasswordField: true,
+                ),
 
-                  SizedBox(
-                    height: 50,
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(18),
-                        )),
-                        onPressed: () => {},
-                        child: const Text(
-                          "Entrar",
-                          style: TextStyle(fontSize: 18),
-                        )),
-                  ),
+                // Botão Entrar
+                SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      )),
+                      onPressed: () => {},
+                      child: const Text(
+                        "Entrar",
+                        style: TextStyle(fontSize: 18),
+                      )),
+                ),
 
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10.0),
-                    child: Align(
-                      alignment: Alignment.centerRight,
-                      child: TextButton(
-                        onPressed: () => {},
-                        child: const Text(
-                          'Esqueceu a senha?',
-                          style: TextStyle(color: Colors.red),
-                        ),
+                // Botão Esqueceu a senha
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                      onPressed: () => {},
+                      child: const Text(
+                        'Esqueceu a senha?',
+                        style: TextStyle(color: Colors.red),
                       ),
                     ),
                   ),
+                ),
 
+                // Divisor
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          color: Colors.grey.withAlpha(90),
+                          thickness: 2,
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 15.0),
+                        child: Text('Ou'),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          color: Colors.grey.withAlpha(90),
+                          thickness: 2,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
 
-
-                ],
-              ),
+                // Botão de novo usuário
+                SizedBox(
+                  height: 50,
+                  child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18)),
+                        side: const BorderSide(
+                          width: 2,
+                          color: Colors.green,
+                        )),
+                    onPressed: () => {},
+                    child: const Text(
+                      'Criar Conta',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                )
+              ],
             ),
           ),
         ],
