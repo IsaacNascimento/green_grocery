516570b59ffdcfcae81686d2f1b2870e09c6b6d5
isaac.nascimento <isaacs.nascimento17@gmail.com>
2023-06-19 20:10:15 -0300

    [Sign Up Screen]: Screen Structre

--- lib/src/auth/sign_up_screen.dart
+++ lib/src/auth/sign_up_screen.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:green_grocer/src/config/custom_colors.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: CustomColors.customSwatchColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Expanded(
+            child: Center(
+              child: Text(
+                'Cadastro',
+                style: TextStyle(color: Colors.white, fontSize: 35),
+              ),
+            ),
+          ),
+
+          // Formulario
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 32,
+              vertical: 40,
+            ),
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(45),
+              )
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
