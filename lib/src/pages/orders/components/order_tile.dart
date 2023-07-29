51b15298fb507cee21e52632ed726433b2391366
isaac.nascimento <isaacs.nascimento17@gmail.com>
2023-07-29 13:20:59 -0300

    [Order Tab]: Order Total Price; Payment Button

--- lib/src/pages/orders/components/order_tile.dart
+++ lib/src/pages/orders/components/order_tile.dart
@@ -32,6 +32,7 @@ class OrderTile extends StatelessWidget {
             ],
           ),
           childrenPadding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+          expandedCrossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             IntrinsicHeight(
               child: Row(
@@ -51,14 +52,14 @@ class OrderTile extends StatelessWidget {
                       ),
                     ),
                   ),
-            
+
                   // Divisão
                   VerticalDivider(
                     color: Colors.grey.shade300,
                     thickness: 2,
                     width: 8,
                   ),
-            
+
                   // Status do Pedido
                   Expanded(
                     flex: 2,
@@ -71,7 +72,43 @@ class OrderTile extends StatelessWidget {
                   ),
                 ],
               ),
-            )
+            ),
+
+            // Total
+            Text.rich(
+              TextSpan(
+                style: const TextStyle(
+                  fontSize: 20,
+                ),
+                children: [
+                  const TextSpan(
+                    text: 'Total ',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  TextSpan(
+                    text: utilsServices.priceToCurrency(order.total),
+                  ),
+                ],
+              ),
+            ),
+
+            // Botão Pagamento
+            Visibility(
+              visible: order.status == 'pending_payment',
+              child: ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                onPressed: () {},
+                icon: Image.asset(
+                  'assets/app_images/pix.png',
+                  height: 18,
+                ),
+                label: const Text('Ver QR Code Pix'),
+              ),
+            ),
           ],
         ),
       ),
