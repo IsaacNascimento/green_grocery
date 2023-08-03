cd873445ae6ab09610687251c36099a4a4b4d477
isaac.nascimento <isaacs.nascimento17@gmail.com>
2023-08-03 19:22:39 -0300

    [Sign Screen]: AuthRepository; Endpoint by a constant;

---
+++ lib/src/pages/auth/repository/auth_repository.dart
@@ -0,0 +1,27 @@
+import 'package:green_grocer/src/constants/endpoints.dart';
+import 'package:green_grocer/src/services/http_manager.dart';
+
+class AuthRepository {
+  final HttpManager _httpManager = HttpManager();
+
+  Future signIn({required String email, required String password}) async {
+    final result = await _httpManager.restRequest(
+      url: EndPoint.signin,
+      method: HttpMethods.post,
+      body: {
+        "email": email,
+        "password": password,
+      },
+    );
+
+    if (result['result'] != null) {
+      return result;
+    } else {
+      Map<String, dynamic> error = {
+        "message": "Ocorreu algum error",
+        "result": result,
+      };
+      return error;
+    }
+  }
+}
