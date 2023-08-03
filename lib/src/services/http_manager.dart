4b3c6c1b37f29aa2cf607ace827dc40e26a60ee1
isaac.nascimento <isaacs.nascimento17@gmail.com>
2023-08-03 16:59:57 -0300

    [Dio Package]: Dio; HttpManager

---
+++ lib/src/services/http_manager.dart
@@ -0,0 +1,46 @@
+import 'package:dio/dio.dart';
+
+abstract class HttpMethods {
+  static const String post = 'POST';
+  static const String get = 'GET';
+  static const String put = 'PUT';
+  static const String putch = 'PUTCH';
+  static const String delete = 'DELETE';
+}
+
+class HttpManager {
+  Future<Map> restRequest({
+    required String url,
+    required String method,
+    Map? headers,
+    Map? body,
+  }) async {
+    // Headers
+    final defaultHeaders = headers?.cast<String, String>() ?? {}
+      ..addAll({
+        'content-type': 'application/json',
+        'accept': 'application/json',
+        'X-Parse-Application-Id': 'wK7GcEjr2V4br5q5mlR1kybQ5dvxMFDX0qtE1d6Y',
+        'X-Parse-REST-API-Key': '2kahi62fkWePLWAwC7k8aMrtQkobogcgkruMxbeB',
+      });
+
+    Dio dio = Dio();
+
+    try {
+      Response response = await dio.request(
+        url,
+        options: Options(method: method, headers: defaultHeaders),
+        data: body,
+      );
+
+      // Retorno backend
+      return response.data;
+    } on DioException catch (e) {
+      // Retorno do error do dio request;
+      return e.response?.data ?? {};
+    } catch (e) {
+      // Retorno de error genérico
+      return {};
+    }
+  }
+}
